e3734afff76868252d9f22eb29b932fe16b5c96d
Richard Gates <mail@richardkentgates.com>
2024-07-28 12:58:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 (function() {
-    document.addEventListener('readystatechange',function(){
-        if (document.readyState == "complete") {
-          const rssURL = 'https://gapcreekmedia.com/feed';
+    rssEmbed = {};
+    rssEmbed.e = {};
+    rssEmbed.f = {};
+    rssEmbed.o = {};
+    rssEmbed.v = {};
+    rssEmbed.f.latestPost = function(rssURL,author,postMax,id){
           fetch(rssURL)
             .then(response => response.text())
             .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-                .then(data => {
-                const feed = data.documentElement;
-                const items = feed.getElementsByTagName('item');
-                for(i=0;i < items.length;i++) {
-                    if( items[i].getElementsByTagName("dc:creator")[0].textContent == 'Richard Gates'){
-                      var tempPub = document.createElement('div');
-                      var tempDoc = document.createElement('div');
-                      var pubDateItems = items[i].getElementsByTagName("pubDate")[0].textContent.split(' ');
-                      tempDoc.appendChild(items[i].getElementsByTagName("description")[0]);
-                      document.getElementById('latest-title').appendChild(items[i].getElementsByTagName("title")[0]);
-                      document.getElementById('latest-pub').appendChild(document.createTextNode(pubDateItems[0] + ' ' + pubDateItems[1] + ' ' + pubDateItems[2] + ' ' + pubDateItems[3]));
-                      document.getElementById('latest-description').innerHTML = tempDoc.textContent;
-                      break;
-                    }
+            .then(data => {
+            rssEmbed.e.feed = data.documentElement;
+            rssEmbed.o.items = rssEmbed.e.feed.getElementsByTagName('item');
+            for(i=0;i < rssEmbed.o.items.length;i++) {
+                if( rssEmbed.o.items[i].getElementsByTagName("dc:creator")[0].textContent == author){
+                      rssEmbed.e.title = document.createElement('h3');
+                      rssEmbed.e.title.innerHTML = rssEmbed.o.items[i].getElementsByTagName("title")[0].innerHTML;                    
+                      rssEmbed.e.tempPub = document.createElement('div');
+                      rssEmbed.e.pub = document.createElement('small');
+                      rssEmbed.v.pubDateItems = rssEmbed.o.items[i].getElementsByTagName("pubDate")[0].textContent.split(' ');
+                      rssEmbed.e.pub.appendChild(document.createTextNode(rssEmbed.v.pubDateItems[0] + ' ' + rssEmbed.v.pubDateItems[1] + ' ' + rssEmbed.v.pubDateItems[2] + ' ' + rssEmbed.v.pubDateItems[3]));
+                      rssEmbed.e.tempDoc = document.createElement('div');
+                      rssEmbed.e.description = document.createElement('p');
+                      rssEmbed.e.tempDoc.appendChild(rssEmbed.o.items[i].getElementsByTagName("description")[0]);
+                      rssEmbed.e.description.innerHTML = rssEmbed.e.tempDoc.textContent;
+                      document.getElementById(id).appendChild(rssEmbed.e.title);
+                      document.getElementById(id).appendChild(rssEmbed.e.pub);
+                      document.getElementById(id).appendChild(rssEmbed.e.description);
+                      if((i+1) == postMax || (i+1) == rssEmbed.o.items.length){ 
+                          break;
+                      }
                 }
-            });
+            }
+        });
+    };
+    document.addEventListener('readystatechange',function(){
+        if (document.readyState == "complete") {
+            rssEmbed.f.latestPost('https://gapcreekmedia.com/feed','Richard Gates',100,'post');
         }
     });
 })();
