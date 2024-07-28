4790adcbd5dc3da1b4e152f5e2d693b9d491fc56
Richard Gates <mail@richardkentgates.com>
2024-07-28 11:51:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,8 @@
                 .then(data => {
                 const feed = data.documentElement;
                 const items = feed.getElementsByTagName('item');
-                var rssStop = 0;
                 for(i=0;i < items.length;i++) {
-                    if( items[i].getElementsByTagName("dc:creator")[0].textContent == 'Richard Gates' && rssStop == 0){
+                    if( items[i].getElementsByTagName("dc:creator")[0].textContent == 'Richard Gates'){
                       var tempPub = document.createElement('div');
                       var tempDoc = document.createElement('div');
                       var pubDateItems = items[i].getElementsByTagName("pubDate")[0].textContent.split(' ');
