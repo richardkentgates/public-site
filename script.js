9c668484e872256d7183326e5d71fb26bd4bee4b
Richard Gates <mail@richardkentgates.com>
2024-08-22 10:02:41 -0500

    Update script.js
    Fixes 2

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
             rssEmbed.o.items = rssEmbed.e.feed.getElementsByTagName('item');
             rssEmbed.e.title = [];
             rssEmbed.e.tempPub = [];
+            rssEmbed.e.pubItems = [];
             rssEmbed.e.pub = [];
             rssEmbed.e.tempDoc = [];
             rssEmbed.e.description = [];
@@ -22,7 +23,8 @@
                       rssEmbed.e.title[i].innerHTML = rssEmbed.o.items[i].getElementsByTagName("title")[0].innerHTML;                    
                       rssEmbed.e.tempPub[i] = document.createElement('div');
                       rssEmbed.e.pub[i] = document.createElement('small');
-                      rssEmbed.e.pub[i].appendChild(document.createTextNode(rssEmbed.o.items[i].getElementsByTagName("pubDate")[0].textContent));
+                      rssEmbed.e.pubItems[i] = rssEmbed.o.items[i].getElementsByTagName("pubDate")[0].textContent.split(' ');
+                      rssEmbed.e.pub[i].appendChild(document.createTextNode(rssEmbed.e.pubItems[i][0] + ' ' + rssEmbed.e.pubItems[i][1] + ' ' + rssEmbed.e.pubItems[i][2] + ' ' + rssEmbed.e.pubItems[i][3]));
                       rssEmbed.e.tempDoc[i] = document.createElement('div');
                       rssEmbed.e.description[i] = document.createElement('p');
                       rssEmbed.e.tempDoc[i].appendChild(rssEmbed.o.items[i].getElementsByTagName("description")[0]);
