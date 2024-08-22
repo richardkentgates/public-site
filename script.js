df668fbce6851764997f4fc708e9f0fa742bac22
Richard Gates <mail@richardkentgates.com>
2024-08-22 09:56:48 -0500

    Update script.js
    Fixes

--- script.js
+++ script.js
@@ -11,21 +11,25 @@
             .then(data => {
             rssEmbed.e.feed = data.documentElement;
             rssEmbed.o.items = rssEmbed.e.feed.getElementsByTagName('item');
+            rssEmbed.e.title = [];
+            rssEmbed.e.tempPub = [];
+            rssEmbed.e.pub = [];
+            rssEmbed.e.tempDoc = [];
+            rssEmbed.e.description = [];
             for(i=0;i < rssEmbed.o.items.length;i++) {
-                if( rssEmbed.o.items[i].getElementsByTagName("dc:creator")[0].textContent == author){
-                      rssEmbed.e.title = document.createElement('h3');
-                      rssEmbed.e.title.innerHTML = rssEmbed.o.items[i].getElementsByTagName("title")[0].innerHTML;                    
-                      rssEmbed.e.tempPub = document.createElement('div');
-                      rssEmbed.e.pub = document.createElement('small');
-                      rssEmbed.v.pubDateItems = rssEmbed.o.items[i].getElementsByTagName("pubDate")[0].textContent.split(' ');
-                      rssEmbed.e.pub.appendChild(document.createTextNode(rssEmbed.v.pubDateItems[0] + ' ' + rssEmbed.v.pubDateItems[1] + ' ' + rssEmbed.v.pubDateItems[2] + ' ' + rssEmbed.v.pubDateItems[3]));
-                      rssEmbed.e.tempDoc = document.createElement('div');
-                      rssEmbed.e.description = document.createElement('p');
-                      rssEmbed.e.tempDoc.appendChild(rssEmbed.o.items[i].getElementsByTagName("description")[0]);
-                      rssEmbed.e.description.innerHTML = rssEmbed.e.tempDoc.textContent;
-                      document.getElementById(id).appendChild(rssEmbed.e.title);
-                      document.getElementById(id).appendChild(rssEmbed.e.pub);
-                      document.getElementById(id).appendChild(rssEmbed.e.description);
+                if( rssEmbed.o.items[i].getElementsByTagName("dc:creator")[0].textContent == author || author === null){
+                      rssEmbed.e.title[i] = document.createElement('h3');
+                      rssEmbed.e.title[i].innerHTML = rssEmbed.o.items[i].getElementsByTagName("title")[0].innerHTML;                    
+                      rssEmbed.e.tempPub[i] = document.createElement('div');
+                      rssEmbed.e.pub[i] = document.createElement('small');
+                      rssEmbed.e.pub[i].appendChild(document.createTextNode(rssEmbed.o.items[i].getElementsByTagName("pubDate")[0].textContent));
+                      rssEmbed.e.tempDoc[i] = document.createElement('div');
+                      rssEmbed.e.description[i] = document.createElement('p');
+                      rssEmbed.e.tempDoc[i].appendChild(rssEmbed.o.items[i].getElementsByTagName("description")[0]);
+                      rssEmbed.e.description[i].innerHTML = rssEmbed.e.tempDoc[i].textContent;
+                      document.getElementById(id).appendChild(rssEmbed.e.title[i]);
+                      document.getElementById(id).appendChild(rssEmbed.e.pub[i]);
+                      document.getElementById(id).appendChild(rssEmbed.e.description[i]);
                       if((i+1) == postMax || (i+1) == rssEmbed.o.items.length){ 
                           break;
                       }
